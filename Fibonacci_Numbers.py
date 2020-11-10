abc82e5a2e5a66444c19dae08a1efc102e4bcd40
Zeaan Pithawala <zeaanpithawala13@gmail.com>
2020-11-10 19:55:05 +0530

    Initial commit

---
+++ Fibonacci_Numbers.py
@@ -0,0 +1,13 @@
+def Fibonacci(end_num):
+    print("0")
+    print("1")
+    a = 0
+    b = 1
+    for i in range(end_num-2):
+        c = a + b
+        print(str(c)+" ")
+        a = b
+        b = c
+
+end_num = int(input(print("Enter the number of fibnonacci numbers you want")))
+Fibonacci(end_num)
